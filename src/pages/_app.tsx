8f9c925f2f573ae25169f26c1798efa63cde40e2
rogeliorv <rogeliorv@gmail.com>
2024-04-20 20:57:45 +0300

    chore: added styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import '@/styles/globals.css';
 // !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
 import '@/styles/colors.css';
 import '@/styles/main.css';
+import '@/styles/vaulDrawerStyles.css';
 
 /**
  * `Layout` component is called in every page using `np` snippets. If you have consistent layout across all page, you can add it here too
