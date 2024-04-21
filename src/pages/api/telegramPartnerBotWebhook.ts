3f1df5475d369996e7bdfbb20a2bd811b7799448
rogeliorv <rogeliorv@gmail.com>
2024-04-21 07:06:34 +0300

    fix: compilation errors

--- src/pages/api/telegramPartnerBotWebhook.ts
+++ src/pages/api/telegramPartnerBotWebhook.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-console */
 import { InlineKeyboardMarkup, Message } from '@grammyjs/types';
 import { NextApiRequest, NextApiResponse } from 'next';
@@ -5,40 +6,54 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { TelegramApiClient } from '@/clients/TelegramApiClient';
 
 class MessageHandler {
-
   private rawRequest: unknown;
   private message: Message;
   private callbackQuery: unknown;
   private telegramApiClient: TelegramApiClient;
+  private appUrl: string;
 
   constructor(request: any) {
-    this.telegramApiClient = new TelegramApiClient(process.env.TELEGRAM_PARTNER_BOT_TOKEN || '');
+    if (!process.env.TELEGRAM_USER_BOT_TOKEN) {
+      throw new Error(
+        'You need to specify TELEGRAM_USER_BOT_TOKEN in your environment'
+      );
+    }
+
+    if (!process.env.NEXT_PUBLIC_APP_URL) {
+      throw new Error(
+        'You need to specify NEXT_PUBLIC_APP_URL in your environment'
+      );
+    }
+
+    this.telegramApiClient = new TelegramApiClient(
+      process.env.TELEGRAM_PARTNER_BOT_TOKEN || ''
+    );
+    this.appUrl = process.env.NEXT_PUBLIC_APP_URL;
     this.rawRequest = request;
     this.message = request.message;
     this.callbackQuery = request.callback_query;
   }
 
   private async sendStartOptions() {
-
     const reply_markup: InlineKeyboardMarkup = {
-      inline_keyboard:[
+      inline_keyboard: [
         [
-        {
-          text: 'Open Map',
-          web_app: { url: `${this.appUrl}/findPartnersMap`},
-        },
-        {
-          text: 'Stop being a human-ATM',
-          callback_data: 'stop_merchant',
-        }
+          {
+            text: 'Open Map',
+            web_app: { url: `${this.appUrl}/findPartnersMap` },
+          },
+          {
+            text: 'Stop being a human-ATM',
+            callback_data: 'stop_merchant',
+          },
+        ],
       ],
-      ]
     };
 
     await this.telegramApiClient.sendMessage(
       this.message.chat.id,
       `You said ${this.message.text}`,
-      reply_markup,
+      reply_markup
     );
   }
 
@@ -46,32 +61,33 @@ class MessageHandler {
   // then we get a callback_query
   private async handleCallbackQuery() {
     console.log('Callback query not implemented yet');
-  };
+  }
 
   // Handles all the messages that come as text from the user
   private async handleMessage() {
     return await this.sendStartOptions();
-  };
+  }
 
   public async handleRequest() {
-    if(this.message) {
+    if (this.message) {
       console.log('Handling message');
       return await this.handleMessage();
-    }
-    else if(this.callbackQuery) {
+    } else if (this.callbackQuery) {
       console.log('Handling callback query');
       return await this.handleCallbackQuery();
     }
   }
 }
 
-
-export default async function handleWebhookRequest(req: NextApiRequest, res: NextApiResponse) {
+export default async function handleWebhookRequest(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
     console.log(`Partner received message on ${Date.now()}`);
     // console.log(req.body);
 
-    if(!req.body) {
+    if (!req.body) {
       console.warn('No body was found in message. Bye');
       res.status(200).end();
       return;
@@ -81,8 +97,7 @@ export default async function handleWebhookRequest(req: NextApiRequest, res: Nex
     const handler = new MessageHandler(req.body);
     await handler.handleRequest();
     res.status(200).end();
-  }
-  catch(error) {
+  } catch (error) {
     // Never fail as to not queue webhook messages
     console.error(error);
     res.status(200).end();
